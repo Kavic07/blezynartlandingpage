7789a930d036e84ee70a64cf8aa5da94c08f8913
Kavic07 <kavicblessing@gmail.com>
2024-03-06 11:37:59 +0100

    changed react logo to mine.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,8 +14,8 @@ const Home = () => {
           Comes to Life
         </h1>
         <p>
-          Become a part of our thriving artistic community by creating an
-          account on BlezynArt.<br></br> Engage in discussions, share your
+          Become a part of our thriving artistic community by joining our
+          community on BlezynArt.<br></br> Engage in discussions, share your
           thoughts, and connect with fellow art lovers.<br></br> Gain insights
           into the artistic process, learn about upcoming events,<br></br> and
           be the first to discover new additions to our gallery.
